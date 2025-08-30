a300fa74cd0d986edd25119ff7b3cc575d031051
Sid9879 <singhsiddharth1438@gmail.com>
2025-08-30 09:17:18 +0530

    static

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -34,26 +34,18 @@ const UserProfile = () => {
             Home
           </Link>
         </li>
-        <li>
-          <div className="flex items-center">
-            <svg className="mx-1 h-4 w-4 text-gray-400 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m9 5 7 7-7 7" />
-            </svg>
-            <a href="#" className="ms-1 text-sm font-medium text-gray-700 hover:text-primary-600 dark:text-gray-400 dark:hover:text-white md:ms-2">My account</a>
-          </div>
-        </li>
         <li aria-current="page">
           <div className="flex items-center">
             <svg className="mx-1 h-4 w-4 text-gray-400 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m9 5 7 7-7 7" />
             </svg>
-            <span className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400 md:ms-2">Account</span>
+            <span className="ms-1 text-sm font-medium text-gray-500 dark:text-gray-400 md:ms-2">View Profile</span>
           </div>
         </li>
       </ol>
     </nav>
     <h2 className="mb-4 text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl md:mb-6">General overview</h2>
-    <div className="grid grid-cols-2 gap-6 border-b border-t border-gray-200 py-4 dark:border-gray-700 md:py-8 lg:grid-cols-4 xl:gap-16">
+    {/* <div className="grid grid-cols-2 gap-6 border-b border-t border-gray-200 py-4 dark:border-gray-700 md:py-8 lg:grid-cols-4 xl:gap-16">
       <div>
         <svg className="mb-2 h-8 w-8 text-gray-400 dark:text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312" />
@@ -134,7 +126,7 @@ const UserProfile = () => {
           vs 1 last 3 months
         </p>
       </div>
-    </div>
+    </div> */}
     <div className="py-4 md:py-8">
       <div className="mb-4 grid gap-4 sm:grid-cols-2 sm:gap-8 lg:gap-16">
         <div className="space-y-4">
@@ -142,7 +134,7 @@ const UserProfile = () => {
             <img  className="h-16 w-16 rounded-lg cursor-pointer" src={userStore.user.profilePic} alt="Helene avatar" />
             <p onClick={handleProfileChanger} title='Update Your Profile' className='cursor-pointer'><FaCamera /></p>
             <div>
-              <span className="mb-2 inline-block rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300"> PRO Account </span>
+              {/* <span className="mb-2 inline-block rounded bg-primary-100 px-2.5 py-0.5 text-xs font-medium text-primary-800 dark:bg-primary-900 dark:text-primary-300"> PRO Account </span> */}
               <h2 className="flex items-center text-xl font-bold leading-none text-gray-900 dark:text-white sm:text-2xl">{userStore.user.name}</h2>
             </div>
           </div>
@@ -175,7 +167,7 @@ const UserProfile = () => {
             <dd className="text-gray-500 dark:text-gray-400">{userStore.user.phoneNumber}</dd>
           </dl>
 
-          <dl>
+          {/* <dl>
             <dt className="mb-1 font-semibold text-gray-900 dark:text-white">Payment Methods</dt>
             <dd className="flex items-center space-x-4 text-gray-500 dark:text-gray-400">
               <div className="flex h-12 w-12 shrink-0 items-center justify-center rounded-lg bg-gray-100 dark:bg-gray-700">
@@ -189,17 +181,17 @@ const UserProfile = () => {
                 </div>
               </div>
             </dd>
-          </dl>
+          </dl> */}
         </div>
       </div>
-      <button type="button" data-modal-target="accountInformationModal2" data-modal-toggle="accountInformationModal2" className="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-auto">
+      {/* <button type="button" data-modal-target="accountInformationModal2" data-modal-toggle="accountInformationModal2" className="inline-flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 sm:w-auto">
         <svg className="-ms-0.5 me-1.5 h-4 w-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" viewBox="0 0 24 24">
           <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
         </svg>
         Edit your data
-      </button>
+      </button> */}
     </div>
-    <div className="rounded-lg border border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:p-8">
+    {/* <div className="rounded-lg border border-gray-200 bg-gray-50 p-4 dark:border-gray-700 dark:bg-gray-800 md:p-8">
       <h3 className="mb-4 text-xl font-semibold text-gray-900 dark:text-white">Latest orders</h3>
       <div className="flex flex-wrap items-center gap-y-4 border-b border-gray-200 pb-4 dark:border-gray-700 md:pb-5">
         <dl className="w-1/2 sm:w-48">
@@ -425,7 +417,7 @@ const UserProfile = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div> */}
   </div>
   {/* Account Information Modal */}
   <div id="accountInformationModal2" tabIndex={-1} aria-hidden="true" className="max-h-auto fixed left-0 right-0 top-0 z-50 hidden h-[calc(100%-1rem)] max-h-full w-full items-center justify-center overflow-y-auto overflow-x-hidden antialiased md:inset-0">
